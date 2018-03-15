0f1f58ca5b14982781d766def99c75d9e6a18e0a
euriphides <euriphides@gmail.com>
2018-03-15 16:14:41 -0600

    Navigation Parameters

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import Courses from './Courses';
 import Teachers from './Teachers';
 import NotFound from './NotFound';
 
+
 const App = () => (
   <BrowserRouter>
     <div className="container">
@@ -22,8 +23,8 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route path="/about" component={ () => <About title='About' />} />
         <Route path="/courses" component={Courses} />
-        <Route path="/teachers" component={Teachers} />
-        <Route path="/featured" component={Featured} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/teachers/:topic/:fname-:lname" component={Featured} />
         <Route component={NotFound} />
       </Switch>
     </div>
