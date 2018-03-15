c4a98965661a22fed7f3f425f39f09ce360d79e3
euriphides <euriphides@gmail.com>
2018-03-15 15:54:09 -0600

    Fully Functional - All Routes in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+// App components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Featured from './Featured';
+import Courses from './Courses';
+import Teachers from './Teachers';
+import NotFound from './NotFound';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={ () => <About title='About' />} />
+        <Route path="/courses" component={Courses} />
+        <Route path="/teachers" component={Teachers} />
+        <Route path="/featured" component={Featured} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
